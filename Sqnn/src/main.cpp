91d28c59daf4e3fc65bec5ed972872327f9cf21b
Christian <s133288@dtu.dk>
2024-02-06 16:22:10 -0500

    added some test data to main to see everything operate

--- Sqnn/src/main.cpp
+++ Sqnn/src/main.cpp
@@ -14,11 +14,39 @@ int main(int argc, char *argv[]){
         kernel_function =
             ::kernel_function<double>(kernel::Gaussian);
 
-    std::cout << "test\n";
+    std::array<std::vector<double>, 20> vectors = {{
+       {36.0, 18.0, 0.0},
+       {18.0, 13.0, 47.0},
+       {19.0, 37.0, 13.0},
+       {27.0, 31.0, 30.0},
+       {28.0, 46.0, 36.0},
+       {28.0, 34.0, 49.0},
+       {15.0, 10.0, 32.0},
+       {33.0, 1.0, 31.0},
+       {35.0, 0.0, 48.0},
+       {11.0, 19.0, 10.0},
+       {41.0, 5.0, 28.0},
+       {41.0, 25.0, 41.0},
+       {41.0, 44.0, 29.0},
+       {6.0, 21.0, 12.0},
+       {38.0, 1.0, 9.0},
+       {24.0, 31.0, 43.0},
+       {25.0, 46.0, 6.0},
+       {9.0, 31.0, 9.0},
+       {40.0, 17.0, 11.0},
+       {41.0, 30.0, 31.0},
+    }};
 
-    std::vector<double> x = {1.0, 2.0, 3.0};
-    std::vector<double> y = {4.0, 5.0, 6.0};
+    double sum = 0.0;
 
-    std::cout << kernel_function(x,y);
+    for(int i = 0; i < 20; i++){
+        for(int j = i + 1; j < 20; j++){
+            sum += kernel_function(vectors[i], vectors[j]);
+        }
+    }
+
+    std::cout << sum << "\n";
+    std::cout << sum/20;
+
 
 }
