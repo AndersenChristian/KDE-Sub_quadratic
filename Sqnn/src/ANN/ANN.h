b7e77b6a30e6e1b2d876feae93b141b554e96330
Christian <s133288@dtu.dk>
2024-02-27 10:57:57 -0500

    tryed getting NGT to work, no luck

--- Sqnn/src/ANN/ANN.h
+++ Sqnn/src/ANN/ANN.h
@@ -5,36 +5,37 @@
 #ifndef KDE_SUB_QUADRATIC_ANN_H
 #define KDE_SUB_QUADRATIC_ANN_H
 
-#include "NGT/Index.h"
+#include <NGT/Index.h>
 
-template <class T>
+template<class T>
 class Ann {
 private:
-    NGT::Index mIndex;
-    NGT::Property mProperty;
+	NGT::Index mIndex;
+	NGT::Property mProperty;
 public:
-    Ann(std::vector<std::vector<T>> data, const int dimension){
-        mProperty.dimension = dimension;
-        //Todo, switch case i guess on types?
-        mProperty.objectType = NGT::ObjectSpace::ObjectType::Float;
-        //Todo, do we always use L2? and does that mean second norm
-        mProperty.distanceType = NGT::Index::Property::DistanceType::DistanceTypeL2;
+	Ann(std::vector<std::vector<T>> data, const int dimension) {
+		mProperty.dimension = dimension;
+		//Todo, switch case i guess on types?
+		mProperty.objectType = NGT::ObjectSpace::ObjectType::Float;
+		//Todo, do we always use L2? and does that mean second norm
+		mProperty.distanceType = NGT::Index::Property::DistanceType::DistanceTypeL2;
 
-        //build index:
-        std::string   path("anng");
-        NGT::Index::create(path, mProperty);
-        NGT::Index    index(path);
-        for(const auto x : data){
-            index.append(x);
-        }
-        index.createIndex(16);
-        index.save();
-    }
+		//build index:
+		std::string path("anng");
+		NGT::Index::create(path, mProperty);
+		NGT::Index index(path);
+		for (const auto x: data) {
+			index.append(x);
+		}
+		index.createIndex(16);
+		index.save();
+	}
 
-    void NNS(std::vector<T> point){
-        if (point.size() != mProperty.dimension) throw std::invalid_argument("size of vector must be equal to the init vector");
-        std::cout << "test";
-    }
+	void NNS(std::vector<T> point) {
+		if (point.size() != mProperty.dimension)
+			throw std::invalid_argument("size of vector must be equal to the init vector");
+		std::cout << "test";
+	}
 };
 
 #endif //KDE_SUB_QUADRATIC_ANN_H
