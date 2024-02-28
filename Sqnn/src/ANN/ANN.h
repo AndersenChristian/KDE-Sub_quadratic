861b64ea759d0754a9cd93c467d4fa52a7d3a3e4
Christian <s133288@dtu.dk>
2024-02-28 15:20:23 -0500

    Building as communication between own system and Mrpt, due to large Header

--- Sqnn/src/ANN/ANN.h
+++ Sqnn/src/ANN/ANN.h
@@ -5,27 +5,24 @@
 #ifndef KDE_SUB_QUADRATIC_ANN_H
 #define KDE_SUB_QUADRATIC_ANN_H
 
-#include <flann/flann.hpp>
-#include <flann/io/hdf5.h>
+#include "Mrpt.h"
 
 template<class T>
 class Ann {
 private:
+	Mrpt *mrpt;
 public:
-	Ann(std::vector<std::vector<T>> data, const int dimension) {
-		//int nn = 3;
-		//flann::Matrix<float> dataset;
-		//flann::Matrix<float> query;
-		//flann::load_from_file(dataset, "dataset.hdf5","dataset");
-		//flann::load_from_file(query, "dataset.hdf5","query");
-		//flann::Matrix<int> indices(new int[query.rows*nn], query.rows, nn);
-		//flann::Matrix<float> dists(new float[query.rows*nn], query.rows, nn);
-// construct an randomized kd-tree index using 4 kd-trees
-		//flann::Index<flann::L2<float> > index(dataset, flann::KDTreeIndexParams(4));
-		//index.buildIndex();
-// do a knn search, using 128 checks
-		//index.knnSearch(query, indices, dists, nn, flann::SearchParams(128));
-		//flann::save_to_file(indices,"result.hdf5","result");
+	Ann(T *data, const int d, const int n, const int k, const float epsilon) {
+		Eigen::MatrixXf dataM(d, n);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < d; j++){
+				dataM(j,i) = data[i*d + j];
+			}
+		}
+		std::cout << dataM.transpose();
+
+		mrpt = new Mrpt(dataM);
+		std::cout <<"\n done \n";
 	}
 
 };
