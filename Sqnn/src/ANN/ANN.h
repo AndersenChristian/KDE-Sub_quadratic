d33ab7bdc76bcd9c034e5c9c533b4f158881b221
Christian <s133288@dtu.dk>
2024-03-13 18:13:12 -0400

    rebuilding the ANN

--- Sqnn/src/ANN/ANN.h
+++ Sqnn/src/ANN/ANN.h
@@ -6,37 +6,27 @@
 #define KDE_SUB_QUADRATIC_ANN_H
 
 #include "Mrpt.h"
+#include "../API/kernelFunction.h"
 
-template<class T>
+template<typename T>
+using node = vector<std::pair<vector<T>,vector<T>>*>;
+
+template<typename T>
 class Ann {
-private:
-	Mrpt *mrpt;
-	int k;
 public:
-	Ann(T *data, const int d, const int n, const int k, const float epsilon, double target_recall) : k(k) {
-		Eigen::MatrixXf dataM(d, n);
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < d; j++) {
-				dataM(j, i) = data[i * d + j];
-			}
-		}
-		mrpt = new Mrpt(dataM);
-		mrpt->grow_autotune(target_recall, k);
-	}
 
-	void exact(const float *q, int *out, float *out_distance = nullptr) {
-		mrpt->exact_knn(q, this->k, out, out_distance);
-	}
+	Ann(T *X, kernelType kernel, double bandWidth, int numberOfTrees) :
+	trees(new node<T>(numberOfTrees)){
 
-	void aprox(const float *q, int *out, float *out_distance = nullptr) {
-		mrpt->query(q, out, out_distance);
-		//mrpt->query(q, out);
 	}
 
 
-	~Ann(){
-		delete mrpt;
-	};
+	~Ann() = default;
+
+private:
+	T *X;
+	node<T> trees;
+
 
 };
 
