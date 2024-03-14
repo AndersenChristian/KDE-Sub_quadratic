96729a816c4cfc000e47f17ec6715de109d375c2
Christian <s133288@dtu.dk>
2024-03-13 20:04:25 -0400

    began building the ann forest

--- Sqnn/src/ANN/ANN.h
+++ Sqnn/src/ANN/ANN.h
@@ -9,21 +9,31 @@
 #include "../API/kernelFunction.h"
 
 template<typename T>
-using node = vector<std::pair<vector<T>,vector<T>>*>;
+using node = vector<std::pair<vector<T>, vector<T>> *>;
 
 template<typename T>
 class Ann {
 public:
 
 	Ann(T *X, kernelType kernel, double bandWidth, int numberOfTrees) :
-	trees(new node<T>(numberOfTrees)){
+			trees(new node<T>(numberOfTrees)) {
+		for(auto *t: trees) t = new node<T>;
+		bool successfullyCreatedTree = false;
+		for (int i = 0; i < numberOfTrees; i = successfullyCreatedTree ? i++ : i) {
+			successfullyCreatedTree = false;
+			makeTree(trees[i], 1);
 
+		}
 	}
 
 
 	~Ann() = default;
 
 private:
+	int makeTree(node<T> *tree, int currentIndex) {
+		
+	}
+
 	T *X;
 	node<T> trees;
 
