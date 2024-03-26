fd3c6826eb81ce56f3ef23b43f7896a93a0a4ba9
Christian <s133288@dtu.dk>
2024-03-26 16:02:23 -0400

    tested all the way up to n=10^5 and d=10^4. need more mem for bigger test

--- Sqnn/src/main.cpp
+++ Sqnn/src/main.cpp
@@ -37,15 +37,17 @@ int main(int argc, char *argv[]) {
 	//basic variable
 	//TODO get as arguments or from file.
 	//TODO bigger n for testing, as the amount of sample required is to high for accurate result.
-	const int n = 10000, d = 2000, k = 100, m = 1000, trees = 100;
-	const float sigma = 100;
+	const int n = 100000, d = 10000, k = 100, m = 30000, trees = 100;
+	const float sigma = 10000;
 
 	Eigen::MatrixXf X = Eigen::MatrixXf(d, n);
 	Eigen::MatrixXf q = Eigen::VectorXf(d);
 	generateRandomMatrix(X, q, d, n);
 
+	double cStart = omp_get_wtime();
 	KdeUsingMrpt kde(X, n, d, m, k, trees, sigma);
-	std::cout << "init finished\n";
+	double cEnd = omp_get_wtime();
+	std::cout << "init finished\nIt took: " << cEnd - cStart << "\n";
 
 
 	float app, exact;
