1490a2d152b4809e5ecd31caeb87bbcca83c3985
Christian <s133288@dtu.dk>
2024-04-05 07:59:12 -0400

    formatting

--- Sqnn/src/kernelFunction.h
+++ Sqnn/src/kernelFunction.h
@@ -15,11 +15,11 @@ using std::vector;
 //dealing with what equations to use
 //
 //Done since I expect that the library used will change later based on efficiency and data types
-namespace mat{
-    using std::sqrt;
-    using std::pow;
-    using std::abs;
-    using std::exp;
+namespace mat {
+	using std::sqrt;
+	using std::pow;
+	using std::abs;
+	using std::exp;
 }
 
 namespace kernel {
@@ -29,57 +29,57 @@ namespace kernel {
 		Laplacian
 	};
 
-    /**
-     * Concept to limit T to arithmetic types
-     * Might be set explicit to some datatypes later
-     *
-     * @tparam T Any type that support arithmatic operations
-     */
-    template<typename T>
-    concept Arithmetic = std::is_arithmetic_v<T>;
+	/**
+	 * Concept to limit T to arithmetic types
+	 * Might be set explicit to some datatypes later
+	 *
+	 * @tparam T Any type that support arithmatic operations
+	 */
+	template<typename T>
+	concept Arithmetic = std::is_arithmetic_v<T>;
 
-    /**
-     * Declaring of the lambda function that is used as the kernel function for the KDE. \n
-     * lambda follows the format of: Lambda((vector<T>, vector<T>, double) -> T) \n
-     * takes 2 vectors of equal dimensions, and a bandwidth sigma and returns the distance. \n
-     * \n
-     * @tparam T support Arithmatic
-     * @param x, y both need to be same size, else it may lead to unwanted behavior.
-     * @param sigma bandwidth, must be greater then 0
-     */
-    template<Arithmetic T>
-    using kernelLambda = std::function<T(const Eigen::VectorXf &x, const Eigen::VectorXf &y, double sigma)>;
+	/**
+	 * Declaring of the lambda function that is used as the kernel function for the KDE. \n
+	 * lambda follows the format of: Lambda((vector<T>, vector<T>, double) -> T) \n
+	 * takes 2 vectors of equal dimensions, and a bandwidth sigma and returns the distance. \n
+	 * \n
+	 * @tparam T support Arithmatic
+	 * @param x, y both need to be same size, else it may lead to unwanted behavior.
+	 * @param sigma bandwidth, must be greater then 0
+	 */
+	template<Arithmetic T>
+	using kernelLambda = std::function<T(const Eigen::VectorXf &x, const Eigen::VectorXf &y, double sigma)>;
 
-    /**
-     * Given an ENUM of kernelType returns the corresponding lambda functions
-     *
-     * @tparam T of Arithmatic
-     * @param kernel kernelType
-     * @return kernelLambda of type T
-     */
-    template<Arithmetic T>
-    //TODO: add sigma as apart of the construction, as that could make it const.
-    inline kernelLambda<T> kernel_function(kernel::type kernel) {
-        switch (kernel) {
-            //Gaussian kernel = e^(-||x-y||^2) where ||x-y||^2 is second norm
-            //Second norm = sqrt(|x0-y0|^2 + ... + |xd-yd|^2)
-					case kernel::type::Gaussian:
-                return kernelLambda<T>([](const Eigen::VectorXf &x, const Eigen::VectorXf &y, double sigma) -> T {
-                    T sum = 0;
-                    for (int i = 0; i < x.size(); i++)
-                        sum += mat::pow(mat::abs(x[i] - y[i]), 2);
-                    sum = mat::sqrt(sum/(2* (sigma * sigma)));
-                    return mat::exp(-sum);
-                });
-					case kernel::type::Exponential:
-                break;
-					case kernel::type::Laplacian:
-                break;
-        }
+	/**
+	 * Given an ENUM of kernelType returns the corresponding lambda functions
+	 *
+	 * @tparam T of Arithmatic
+	 * @param kernel kernelType
+	 * @return kernelLambda of type T
+	 */
+	template<Arithmetic T>
+	//TODO: add sigma as apart of the construction, as that could make it const.
+	inline kernelLambda<T> kernel_function(kernel::type kernel) {
+		switch (kernel) {
+			//Gaussian kernel = e^(-||x-y||^2) where ||x-y||^2 is second norm
+			//Second norm = sqrt(|x0-y0|^2 + ... + |xd-yd|^2)
+			case kernel::type::Gaussian:
+				return kernelLambda<T>([](const Eigen::VectorXf &x, const Eigen::VectorXf &y, const double sigma) -> T {
+					T sum = 0;
+					for (int i = 0; i < x.size(); i++)
+						sum += mat::pow(mat::abs(x[i] - y[i]), 2);
+					sum = mat::sqrt(sum / (2 * (sigma * sigma)));
+					return mat::exp(-sum);
+				});
+			case kernel::type::Exponential:
+				break;
+			case kernel::type::Laplacian:
+				break;
+		}
 
-        //default (unreachable only there to avoid compiler warning)
-        return [](const Eigen::VectorXf &x, const Eigen::VectorXf &y, double) -> T { return T{}; };
-    }
+		//default (unreachable only there to avoid compiler warning)
+		return [](const Eigen::VectorXf &x, const Eigen::VectorXf &y, double) -> T { return T{}; };
+	}
 }
 
 #endif //KDE_SUB_QUADRATIC_KERNELFUNCTION_H
