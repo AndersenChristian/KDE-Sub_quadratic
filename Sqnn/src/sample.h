0f32ae1cb31887cdb52e02966fb1c074dbc0c4f6
Christian <s133288@dtu.dk>
2024-04-08 14:35:10 -0400

    made file and added to CMakeLists

---
+++ Sqnn/src/sample.h
@@ -0,0 +1,15 @@
+//
+// Created by cj on 8-4-24.
+//
+
+#ifndef KDE_SUB_QUADRATIC_SAMPLE_H
+#define KDE_SUB_QUADRATIC_SAMPLE_H
+
+#include <vector>
+#include <Eigen/Dense>
+
+void vertexSample(std::vector<int> &list, const Eigen::VectorXf &data, const int samples){
+	std::vector
+}
+
+#endif //KDE_SUB_QUADRATIC_SAMPLE_H
